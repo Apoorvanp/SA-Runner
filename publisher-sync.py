dc6af11be3c8fbd4bec120beaa25d9f321d64320
apoorvanp <npapoorva1997@gmail.com>
2023-01-08 20:29:48 +0100

    Basic working version

--- publisher-sync.py
+++ publisher-sync.py
@@ -1,11 +1,12 @@
 import random
 import requests
+import time
 
 while True:
-    for(community in range(1,10)):
-        for(house in range(1,5)):
+    for community in range(1,10):
+        for house in range(1,5):
             production = random.uniform(2, 10)
             payload = {"communityId": community,"houseId": house,"energyProduced":  production}
-            response = requests.post(url='host.docker.internal:8080/api/v1/marketplace/produce', data=payload)
-            print("Produced {} for community: {}, house: {}, {}", production, community, house response.status_code)
+            response = requests.post(url='http://host.docker.internal:8080/api/v1/marketplace/produce', json=payload)
+            print("Produced {} for community: {}, house: {}, {}", production, community, house, response.status_code)
     time.sleep(2)
