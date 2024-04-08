d94313f01079589354c0536716ae2cd480ffcb90
Apoorva NP <npapoorva1997@gmail.com>
2024-04-08 21:03:31 +0200

    Use host from env variable

--- consumer-sync.py
+++ consumer-sync.py
@@ -1,12 +1,15 @@
+import os
 import random
 import requests
 import time
+
+host = os.environ['HOST']
 
 while True:
     for community in range(1,10):
         for house in range(1,5):
             consumption = random.uniform(2, 10)
             payload = {"communityId": community,"houseId": house,"energyNeed":  consumption}
-            response = requests.post(url='http://host.docker.internal:8080/api/v1/consumptions', json=payload)
+            response = requests.post(url=f'http://{host}:8080/api/v1/consumptions', json=payload)
             print(f'Consume {consumption} for community: {community}, house: {house} was {response.text}')
     time.sleep(2)
--- producer-sync.py
+++ producer-sync.py
@@ -1,12 +1,14 @@
+import os
 import random
 import requests
 import time
 
+host = os.environ['HOST']
 while True:
     for community in range(1,10):
         for house in range(1,5):
             production = random.uniform(2, 10)
             payload = {"communityId": community,"houseId": house,"energyProduced":  production}
-            response = requests.post(url='http://host.docker.internal:8080/api/v1/productions', json=payload)
+            response = requests.post(url=f'http://{host}:8080/api/v1/productions', json=payload)
             print(f'Produced {production} for community: {community}, house: {house}')
     time.sleep(2)
