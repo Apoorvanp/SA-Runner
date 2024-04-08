3198a490db5c730a328841315b9e3127413b5a7e
Apoorva NP <npapoorva1997@gmail.com>
2024-04-08 21:07:49 +0200

    Remove port

--- consumer-sync.py
+++ consumer-sync.py
@@ -10,6 +10,6 @@ while True:
         for house in range(1,5):
             consumption = random.uniform(2, 10)
             payload = {"communityId": community,"houseId": house,"energyNeed":  consumption}
-            response = requests.post(url=f'http://{host}:8080/api/v1/consumptions', json=payload)
+            response = requests.post(url=f'http://{host}/api/v1/consumptions', json=payload)
             print(f'Consume {consumption} for community: {community}, house: {house} was {response.text}')
     time.sleep(2)
